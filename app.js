d916e758ab05fe4f96abe9c71a419f1fa34ebb35
SHATHISH-07 <satheesh12072004@gmail.com>
2025-03-08 17:52:29 +0530

    solved the error

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-// get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
-
 app.get('/health', (req, res) => {
   res.status(200).send('ok')
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 app.listen(PORT, () => {
